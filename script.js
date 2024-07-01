d478d5b110c8091c7bf2b9765794a5b7e6c2026b
rafaelmoraesx <rafael20072011@gmail.com>
2024-07-01 17:13:42 -0300

    bootstrap

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function startProgress() {
-    var elem = document.getElementById("progress-bar");
-    var width = 0;
-    var id = setInterval(frame, 50);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
-            elem.innerHTML = width * 1  + '%';
-        }
-    }
+function updateProgress() {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+    const progress = document.getElementById('progress-bar');
+
+   
+    let filled = 0;
+    if (name !== '') filled++;
+    if (email !== '') filled++;
+    if (message !== '') filled++;
+
+    
+    const progressWidth = (filled / 3) * 100; 
+    progress.style.width = progressWidth + '%';
+    progress.textContent = progressWidth.toFixed(0) + '%';
 }
